b66127d0345b16272ccfdf69c3c608ea9211f1f1
Kye Moon <kyemoondevelopment@gmail.com>
2022-11-04 23:07:20 +1100

    sss

--- src/pages/HomePageComponents/Nav.tsx
+++ src/pages/HomePageComponents/Nav.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Logo from "../../../public/Logomark.png"
+import Logo from "../../../public/logomark.png"
 import { Link } from "gatsby"
 export default function Nav() {
   return (
