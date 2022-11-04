522923322a7fa410da1bbcdc64f21ad30c95fa0d
Kye Moon <kyemoondevelopment@gmail.com>
2022-11-04 23:37:15 +1100

    ssssssss

--- src/pages/HomePageComponents/Nav.tsx
+++ src/pages/HomePageComponents/Nav.tsx
@@ -4,13 +4,10 @@ import { Link } from "gatsby"
 export default function Nav() {
   return (
     <div className="navbar bg-base-100 py-4">
-      <div className="navbar-start flex flex-row space-x-4 px-12">
+      <div className="navbar-start flex space-x-4 px-12">
         <img src={Logo} className="h-20 w-20" />
-        <a className="btn btn-ghost text-3xl normal-case text-secondary">
-          Deep Purple
-        </a>
+        <div className=" text-3xl  text-secondary">Deep Purple</div>
       </div>
-      <div className="navbar-center hidden lg:flex"></div>
       <div className="navbar-end">
         <Link to="https://dev1410.dbyl8h7nhmtvn.amplifyapp.com/">
           <button className="btn btn-outline btn-secondary px-12">Login</button>
