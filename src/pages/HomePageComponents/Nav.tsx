df683819c72d71f033554d72150b27b06fffbf93
Kye Moon <kyemoondevelopment@gmail.com>
2022-11-04 23:45:23 +1100

    bhdfgg

--- src/pages/HomePageComponents/Nav.tsx
+++ src/pages/HomePageComponents/Nav.tsx
@@ -6,12 +6,17 @@ export default function Nav() {
     <div className="navbar bg-base-100 py-4">
       <div className="navbar-start flex space-x-4 px-12">
         <img src={Logo} className="h-20 w-20" />
-        <div className=" text-3xl  text-secondary">Deep Purple</div>
+        <h1 className=" text-3xl  text-secondary">Deep Purple</h1>
       </div>
       <div className="navbar-end">
-        <Link to="https://dev1410.dbyl8h7nhmtvn.amplifyapp.com/">
-          <button className="btn btn-outline btn-secondary px-12">Login</button>
-        </Link>
+        <div className="flex justify-end">
+          {" "}
+          <Link to="https://dev1410.dbyl8h7nhmtvn.amplifyapp.com/">
+            <button className="btn btn-outline btn-secondary px-12">
+              Login
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   )
